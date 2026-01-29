9d1decb3c98390bf00a87023e20c7072c453500b
B. Isabella Morel <isabellamorelortiz244@gmail.com>
2026-01-29 14:26:21 -0400

    finished

--- calc.py
+++ calc.py
@@ -3,8 +3,10 @@ while True:
     print("----------------")
     print("1. Addition")
     print("2. Subtraction")
-    print("3. Exit")
-    choice = input("What would you like to do? (type 1, 2 or 3): ")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+    choice = input("What would you like to do? (type 1, 2, 3, 4 or 5): ")
     if choice == '1':
         x = int(input("Enter first number: "))
         y = int(input("Enter second number: "))
@@ -16,5 +18,18 @@ while True:
         subtraction = x - y
         print("The difference is: " + str(subtraction))
     elif choice == '3':
+        x = int(input("Enter first number: "))
+        y = int(input("Enter second number: "))
+        multiplication = x * y
+        print("The product is: " + str(multiplication))
+    elif choice == '4':
+        x = int(input("Enter first number: "))
+        y = int(input("Enter second number: "))
+        if y == 0:
+            print("Error: Division by zero is not allowed.")
+        else:
+            division = x / y
+            print("The quotient is: " + str(division))
+    elif choice == '5':
         print("Exiting. Goodbye.")
         break
